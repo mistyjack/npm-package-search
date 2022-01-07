ad7df6abc70376e22a5f67f12c5a98b7fbe796dd
mistyjack <oladepomustafa@gmail.com>
2022-01-07 23:51:12 +0100

    add fetch package skeleton

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -2,7 +2,9 @@ import { combineReducers } from 'redux';
 import repositoriesReducer from './repositoriesReducer';
 
 const reducers = combineReducers({
-  respositories: repositoriesReducer,
+  repositories: repositoriesReducer,
 });
 
 export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
