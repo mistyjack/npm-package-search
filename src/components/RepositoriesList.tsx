0ec31e08a489d0b667af65d186d9489b41f4452d
mistyjack <oladepomustafa@gmail.com>
2022-01-07 23:06:10 +0100

    add redux structure

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+const RepositoriesList: FC = () => {
+  return (
+    <div>
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList;
